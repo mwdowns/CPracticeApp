0a42a249c571655e305ed3679105dbcf88b1958a
Matthew Downs <mwdowns@gmail.com>
2017-10-06 18:29:27 -0400

    Add project files.

---
+++ CPracticeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CPracticeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
